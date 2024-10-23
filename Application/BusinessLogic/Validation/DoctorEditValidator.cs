9c308ef258e1211ff6b2aa6d63dedf658cfaa76b
Vladislav Lunev <vlclassroom95@gmail.com>
2024-10-23 17:20:30 +0700

    feat(application): add validators for doctor registration request and doctor edit request

---
+++ Application/BusinessLogic/Validation/DoctorEditValidator.cs
@@ -0,0 +1,28 @@
+using Application.Dto;
+using FluentValidation;
+
+namespace Application.BusinessLogic.Validation;
+
+public class DoctorEditValidator : AbstractValidator<DoctorEditRequest>
+{
+    public DoctorEditValidator()
+    {
+        RuleFor(doctor => doctor.email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+        
+        RuleFor(doctor => doctor.name)
+            .NotEmpty().WithMessage("Name is required");
+        
+        RuleFor(doctor => doctor.birthday)
+            .NotEmpty().WithMessage("Birthday is required")
+            .LessThan(DateTime.Today).WithMessage("Birthday must be in the past");
+        
+        RuleFor(doctor => doctor.gender)
+            .IsInEnum().WithMessage("Invalid gender value");
+        
+        RuleFor(doctor => doctor.phone)
+            .NotEmpty().WithMessage("Phone is required")
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Phone number is not valid");
+    }
+}
