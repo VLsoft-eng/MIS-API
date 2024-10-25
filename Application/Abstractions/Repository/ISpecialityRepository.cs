1c855c229fb494360db36dc9e3c0700d0ed581c0
Vladislav Lunev <vlclassroom95@gmail.com>
2024-10-25 22:26:49 +0700

    feat(application): update speciality repository abstraction

--- Application/Abstractions/Repository/ISpecialityRepository.cs
+++ Application/Abstractions/Repository/ISpecialityRepository.cs
@@ -6,5 +6,6 @@ public interface ISpecialityRepository
 {
     Task Create(Speciality speciality);
     Task<Speciality?> GetById(Guid id);
-    Task<IEnumerable<Speciality>> GetByNameAndParams(string name, int page = 1, int size = 5);
+    Task<List<Speciality>> GetByNameAndParams(string name, int page = 1, int size = 5);
+    Task<int> GetCountByName(string name);
 }
