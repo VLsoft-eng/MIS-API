a50905630e260454e38b8452e6464a741b4b0db9
Vladislav Lunev <vlclassroom95@gmail.com>
2024-10-26 21:42:49 +0700

    update(application): update doctor edit and registration validators rules

--- Application/BusinessLogic/Validation/DoctorRegistrationValidator.cs
+++ Application/BusinessLogic/Validation/DoctorRegistrationValidator.cs
@@ -16,7 +16,7 @@ public class DoctorRegistrationValidator : AbstractValidator<DoctorRegistrationR
             .EmailAddress().WithMessage("Invalid email format");
         RuleFor(doctor => doctor.birthday)
             .NotEmpty().WithMessage("Birthday is required")
-            .LessThan(DateTime.Today).WithMessage("Birthday must be in the past");
+            .LessThan(DateTime.UtcNow).WithMessage("Birthday must be in the past");
         RuleFor(doctor => doctor.gender)
             .IsInEnum().WithMessage("Invalid gender value");
         RuleFor(doctor => doctor.phone)
