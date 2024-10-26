21f96df0cc320488d2e3adb29e1f8ee0e400029e
Vladislav Lunev <vlclassroom95@gmail.com>
2024-10-27 01:03:16 +0700

    feat(application): add conclusion and diagnosis type enums

---
+++ Domain/Enums/DiagnosisType.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+
+namespace Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum DiagnosisType
+{
+    Main,
+    Additional,
+    Complication
+}
