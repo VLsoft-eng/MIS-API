9eb7256989c68c5430b9e56d8fb9b8da4747ef80
Vladislav Lunev <vlclassroom95@gmail.com>
2024-10-30 22:51:27 +0700

    feat(api): add report controller

---
+++ Api/Controller/ReportController.cs
@@ -0,0 +1,33 @@
+using Application.Abstractions.Service;
+using Application.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller;
+
+[ApiController]
+[Route("api/report")]
+public class ReportController : ControllerBase
+{
+    private IReportService _reportService;
+
+    public ReportController(IReportService reportService)
+    {
+        _reportService = reportService;
+    }
+
+    [Authorize]
+    [HttpGet("icdrootsreport")]
+    public async Task<IcdRootsReportDto> GetIcdRootsReport(
+        [FromQuery] DateTime start,
+        [FromQuery] DateTime end,
+        [FromQuery] List<Guid>? icdRoots)
+    {
+        if (icdRoots == null || !icdRoots.Any())
+        {
+            icdRoots = new List<Guid>();
+        }
+        
+        return await _reportService.GetReport(start, end, icdRoots);
+    }
+}
